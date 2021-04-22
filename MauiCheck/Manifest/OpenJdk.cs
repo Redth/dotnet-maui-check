956851ccb711eb0dcf767aa50d56174f50aa4d9c
Jonathan Dick <jond@hey.com>
2021-04-21 21:28:29 -0400

    Manifest model changes for jdk

---
+++ MauiCheck/Manifest/OpenJdk.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace DotNetCheck.Manifest
+{
+	public partial class OpenJdk
+	{
+		[JsonProperty("urls")]
+		public Urls Urls { get; set; }
+
+		[JsonIgnore]
+		public System.Uri Url
+			=> Urls?.Get(Version);
+
+		[JsonProperty("version")]
+		public string Version { get; set; }
+
+		[JsonProperty("requireExact")]
+		public bool RequireExact { get; set; }
+	}
+}
